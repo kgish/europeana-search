9e752ff59d6ea3fce3bfc86a8d93b58828ce034f
Kiffin Gish <kiffin.gish@planet.nl>
2017-07-04 22:50:22 +0200

    Entity search.

--- app/controllers/entities.js
+++ app/controllers/entities.js
@@ -12,7 +12,7 @@ export default Ember.Controller.extend({
         'Place'
     ],
 
-    type: 'All',
+    type: 'Agent',
 
     loadingResults: false,
 
@@ -22,26 +22,38 @@ export default Ember.Controller.extend({
 // namespace = base
 
     actions: {
+        reset() {
+            this.set('char', 'A');
+            this.set('type', 'Agent');
+            this.set('entities', []);
+            this.set('loadingResults', false);
+            this._selectChar('A');
+        },
         selectChar(char) {
-            let type = this.get('type'),
-                url = `${config.APP.API_HOST_TEST}/entity/suggest?wskey=${config.APP.API_KEY_ENTITIES}&text=${char}&language=en&type=${type}&rows=10`;
-            this.set('char', char);
-            console.log(url);
-            this.set('loadingResults', true);
-            Ember.$.get(url).then(
-                data => {
-                    console.log(data);
-                    this.set('loadingResults', false);
-                    this.set('entities', data.contains);
-                },
-                error => {
-                    console.error(error);
-                    this.set('loadingResults', false);
-                }
-            );
+            this._selectChar(char);
         },
         selectType(type) {
             this.set('type', type);
         }
+    },
+
+    _selectChar(char) {
+        let type = this.get('type'),
+            url = `${config.APP.API_HOST_TEST}/entity/suggest?wskey=${config.APP.API_KEY_ENTITIES}&text=${char}&language=en&type=${type}&rows=10`;
+        this.set('char', char);
+        console.log(url);
+        this.set('loadingResults', true);
+        Ember.$.get(url).then(
+            data => {
+                console.log(data);
+                this.set('loadingResults', false);
+
+                this.set('entities', data.contains);
+            },
+            error => {
+                console.error(error);
+                this.set('loadingResults', false);
+            }
+        );
     }
 });
--- app/routes/entities.js
+++ app/routes/entities.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    activate() {
+        this._super.apply(this, ...arguments);
+        this.controllerFor(this.routeName).send('reset');
+    }
 });
